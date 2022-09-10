0991f37c61876b5e7ee316e3e24012a5fcf9a150
Michael Davis <jrmdd1496@gmail.com>
2022-09-10 12:52:57 -0500

    Originally did not have db tracked. Began to track and re commited.

---
+++ src/Workout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Workout () {
+
+    return (
+        <div className="workout">
+            <h2 className="workout-name"></h2>
+            <h5 className="workout-type"></h5>
+        </div>
+    )
+
+}
+
+export default Workout;
