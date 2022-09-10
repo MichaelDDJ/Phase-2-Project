5bab4da0d91706f6cc00ddb2a9f58fb3c61ccf64
Michael Davis <jrmdd1496@gmail.com>
2022-09-10 12:43:32 -0500

    Was able to get backend set up and pull info from database

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import React, {useEffect, useState} from 'react';
 import Header from './Header';
 import Main from './Main';
 import {Switch, Link, Route} from "react-router-dom";
 
 function App() {
+
+  const [workouts, setWorkouts] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/workouts")
+    .then((r) => r.json())
+    .then(data => setWorkouts(data))
+  }, [])
+
+  console.log(workouts)
+
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main workouts={workouts} />
     </div>
   );
 }
